9a30c5e41a5b865de9a5e567b424e19d578d699c
Yan <yancertuche@gmail.com>
2021-03-25 14:49:08 -0500

    using join to print array

--- src/Components/Fibo.jsx
+++ src/Components/Fibo.jsx
@@ -4,18 +4,40 @@ function Fibo() {
     const [param, setParam] = useState('0')
     const [result, setResult] =useState([])
     const [error, setError] = useState(false)
-    const regex = new RegExp("^([0-9])*$")
+    const regex = new RegExp("^([1-9])*$")
+
+    function Fibonacci (limit){
+        var a = 0
+        var b = 1
+        var sum = 0
+        var result =[]
+        for(var i=0;i <= limit-1; i++){
+            if(i === 0){
+                result.push(1)
+            }
+            else{
+                sum = a + b
+                result.push(sum)
+                a = b
+                b = sum
+            }
+        }
+        return result
+    };
 
     function fiboClick() {
         console.log(param)
+        setError(false)
         if(regex.test(param)){
             console.log("paso")
-            let result = []
-            console.log(param, result);
+            let result = Fibonacci(param)
+            console.log(param,result)
+            setResult(result)
         }else{
             setError(true)
         }
     }
+
     function fiboChange(e){
         e.preventDefault()
         setParam(e.target.value)
@@ -24,13 +46,12 @@ function Fibo() {
     return (
       <div>
         <h4>Fibonacci</h4>
-        <form>
         <input type="text" onChange={fiboChange}></input>
         <button type="submit" onClick={fiboClick}>Ejecutar</button>
-        </form>
         <div>
             <p>respuesta</p>
-            {error === true ? <p>'Número no valido, es menor a cero'</p> : <p>arreglo respuesta</p>}
+            {error === true ? <p>'Número no valido, debe ser mayor o igual a 1'</p> : 
+                <p>{"(" + result.join(', ') + ")"}</p>}
         </div>
 
       </div>

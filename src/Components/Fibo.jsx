bb734e448aa131405851f816acb2ed9eeddccb59
Yan <yancertuche@gmail.com>
2021-03-25 11:29:16 -0500

    catch input

--- src/Components/Fibo.jsx
+++ src/Components/Fibo.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 
 function Fibo() {
+    const [param, setParam] = useState(0)
+    const [result, setResult] =useState([])
+    const [error, setError] = useState(false)
+
+    function fiboClick() {
+        console.log(param);
+    }
+    function fiboChange(e){
+        setParam(e.target.value)
+    }
+
     return (
       <div>
-        <input type="text"></input>
-        <button>Ejecutar</button>
+        <h4>Fibbonacci</h4>
+        <form>
+        <input type="text" onChange={fiboChange}></input>
+        <button type="submit" onClick={fiboClick}>Ejecutar</button>
+        </form>
+        <div>
+            <p>respuesta</p>
+            {error === true ? <p>'Número no valido, es menor a cero'</p> : <p>arreglo respuesta</p>}
+        </div>
+
       </div>
     );
 }

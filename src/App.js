37d5141833d7809c9d90326cfa327cb6d9715615
Yan <yancertuche@gmail.com>
2021-03-27 14:53:43 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import '../src/Styles/appStyle.css';
 import Fibo from './Components/Fibo';
 import Mult from './Components/Mult';
 import Card from './Components/Card';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="containerApp">
-      <Fibo></Fibo>
-      <Mult></Mult>
-      
-      <Card></Card>
+    <Router>
+        <div>
+          <h4>Prueba Técnica AKELAB - Por : Yan Carlos Certuce Grueso</h4>
+          <ul>
+            <li>
+              <Link to="/puntoUno">Punto 1</Link>
+            </li>
+            <li>
+              <Link to="/puntoDos">Punto 2</Link>
+            </li>
+            <li>
+              <Link to="/puntoTres">Punto 3</Link>
+            </li>
+          </ul>
+      <Switch>
+        <Route path="/puntoUno">  
+          <div className="containerApp">
+            <Fibo></Fibo>
+          </div>
+        </Route>
+        <Route path="/puntoDos">
+          <div className="containerApp">
+            <Mult></Mult>
+          </div>
+        </Route>
+        <Route path="/puntoTres">
+          <div className="containerApp">
+            <Card></Card>
+          </div>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 

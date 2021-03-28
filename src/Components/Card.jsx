07694c68959b1127d760707d69bd381dddb34c93
Yan <yancertuche@gmail.com>
2021-03-28 13:25:42 -0500

    using props

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,39 +1,40 @@
 import '../Styles/cardStyle.css';
 import '../Icons/Star3.png'
-function Card(){
+function Card(props){
     return(
         <div className="card">
 
             <div className="header">
                 <div className="title">
-                    <label>El Club de la Lucha (1999)</label>
+                    <label>{props.Title || "Title"}  {props.Year || "(año)"}</label>
                 </div>
             </div>
 
             <div className="img">
-            <img width="100%"  alt='hola' src='https://image.tmdb.org/t/p/w500/1yWmCAIGSVNuTOGyz9F48W9g1Ux.jpg'></img>
+            <img width="100%"  alt='hola' src={props.ImgPath ||'https://image.tmdb.org/t/p/w500/1yWmCAIGSVNuTOGyz9F48W9g1Ux.jpg'}></img>
             </div>
 
             <div className="right">
                 <div className="overview">
-                    Un joven sin ilusiones lucha contra su insomnio, consecuencia quizás de su hastío por su gris y rutinaria vida. En un viaje en avión conoce a Tyler Durden, un carismático vendedor de jabón que sostiene una filosofía muy particular: el perfeccionismo es cosa de gentes débiles; en cambio, la autodestrucción es lo único que hace que realmente la vida merezca la pena. Ambos deciden entonces formar un club secreto de lucha donde descargar sus frustaciones y su ira que tendrá un éxito arrollador.
+                    {props.Description || "descripción"}
                 </div>
                 <div className="description">
                     <div>
                         <label className="label">Titulo: </label>
-                        <label>titulo pelicula</label>
+                        <label>{props.Title || "titulo pelicula"}</label>
                     </div>
                     <div>
                         <label className="label">Calificación: </label>
+                        <label>{props.Vote || "8.5"}</label>
                         <img className="star" alt ="icon" src='../Icons/Star3.png'></img>
                     </div>
                     <div>
-                        <label className="label">Genero: </label>
-                        <label>los generos</label>
+                        <label className="label">Género: </label>
+                        <label>{props.Genre || "los generos"}</label>
                     </div>
                     <div>
                         <label className="label">Fecha de realización: </label>
-                        <label>fecha</label>
+                        <label>{props.Date || "fecha" }</label>
                     </div>
                 </div>
             </div>
